4b7ca80bcc6acfb45acab264907bfd0ea08d4743
Clarice <claricostello@gmail.com>
2018-11-26 20:03:54 -0800

    passed test 4

--- code-challenges/301/challenges-01.test.js
+++ code-challenges/301/challenges-01.test.js
@@ -85,7 +85,11 @@ Write a function named removeWithForEach that produces the same output as challe
 
 const removeWithForEach = (arr, callback) => {
   // Solution code here...
-}
+  arr.forEach(function(element){
+    callback(element,arr);
+  });
+  return arr;
+};
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 5
