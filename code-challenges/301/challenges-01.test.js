da3cdf7b3064c42ad0ae6b4111891b4518814b4a
Clarice <claricostello@gmail.com>
2018-11-26 20:25:49 -0800

    completed 7th challenge

--- code-challenges/301/challenges-01.test.js
+++ code-challenges/301/challenges-01.test.js
@@ -154,8 +154,21 @@ Return the resulting output array.
 ------------------------------------------------------------------------------------------------ */
 
 const fizzbuzz = (arr) => {
+  let output = [];
+  arr.forEach(function(element){
+    if (element % 3 === 0 && element % 5 !== 0){
+      output.push('Fizz');
+    } else if (element % 5 === 0 && element % 3 !== 0) {
+      output.push('Buzz');
+    } else if (element % 3 === 0 && element % 5 === 0) {
+      output.push('Fizz Buzz');
+    } else {
+      output.push(element);
+    }
+  });
   // Solution code here...
-}
+  return output;
+};
 
 /* ------------------------------------------------------------------------------------------------
 TESTS
