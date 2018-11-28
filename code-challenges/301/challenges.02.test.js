2e8d6eb2f7e6566b6519b76643c1d336355f2a36
Clarice <claricostello@gmail.com>
2018-11-28 08:40:26 -0800

    passed first test

--- code-challenges/301/challenges.02.test.js
+++ code-challenges/301/challenges.02.test.js
@@ -8,7 +8,8 @@ Write a function that appends ' The end.' to a string, and returns the modified 
 
 const appendTheEnd = (str) => {
     // Solution code here...
-    return str;
+    let modifiedString = str + ' The end.';
+    return modifiedString;
   }
   
   /* ------------------------------------------------------------------------------------------------
