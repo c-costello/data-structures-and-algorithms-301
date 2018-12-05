5157bffae31e03c1e172915500d3e95ff0edd235
Clarice <claricostello@gmail.com>
2018-12-04 17:20:01 -0800

    passed first challenge

--- code-challenges/301/07_challenge/challenges-07.test.js
+++ code-challenges/301/07_challenge/challenges-07.test.js
@@ -12,6 +12,10 @@ For example, twoToThe([1,2,3]) returns [2,4,8] because 2 ^ 1 = 2, 2 ^ 2 = 4, and
 
 const forLoopTwoToThe = (arr) => {
   // Solution code here...
+  for(let i = 0; i < arr.length; i++){
+    arr[i] =  Math.pow(2, arr[i]);
+  }
+  return arr;
 };
 
 /* ------------------------------------------------------------------------------------------------
