e9e902c5b917c3264d5c041778192fa048761eb2
Clarice <claricostello@gmail.com>
2018-12-04 17:43:49 -0800

    passed sixth challenge

--- code-challenges/301/07_challenge/challenges-07.test.js
+++ code-challenges/301/07_challenge/challenges-07.test.js
@@ -75,14 +75,14 @@ For example: evenOdd([1,2,3]) returns ['odd','even','odd'].
 const evenOdd = (arr) => {
   // Solution code here...
   let newArr = arr.map( x =>{
-        if(x % 2 === 0){
-            return 'even';
-        } else if (x % 2 === 1){
-            return 'odd'
-        } else {
-            return 'N/A'
-        }
-  })
+    if(x % 2 === 0){
+      return 'even';
+    } else if (x % 2 === 1){
+      return 'odd';
+    } else {
+      return 'N/A';
+    }
+  });
   return newArr;
 };
 
@@ -130,6 +130,10 @@ const snorlaxAbilities = {
 
 const extractAbilities = (arr) => {
   // Solution code here...
+  let newArr = arr.map(x => {
+    return x.ability.name;
+  });
+  return newArr;
 };
 
 /* ------------------------------------------------------------------------------------------------
