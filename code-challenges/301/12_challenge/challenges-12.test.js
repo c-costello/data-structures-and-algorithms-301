77db28f18beeccfb821b5581875aba575fc31f45
Clarice <claricostello@gmail.com>
2018-12-11 17:24:46 -0800

    passed first, second and third test

--- code-challenges/301/12_challenge/challenges-12.test.js
+++ code-challenges/301/12_challenge/challenges-12.test.js
@@ -82,7 +82,8 @@ const errands = [
 
 const howManyTreats = (arr) => {
   // Solution code here...
-}
+  return arr[2].items[1].quantity;
+};
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 4
